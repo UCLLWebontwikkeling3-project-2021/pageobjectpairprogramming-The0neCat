673d6ea6221d6d2b20bf26b48544cdfbb9f26573
Lennert Nowak <lennert.nowak@student.ucll.be>
2020-12-31 18:15:47 +0100

    fout omgemerkt

--- test/java/HomePage.java
+++ test/java/HomePage.java
@@ -14,6 +14,15 @@ public class HomePage extends Page{
     @FindBy(id="logout")
     WebElement button;
 
+    @FindBy(id="login")
+    WebElement loginbutton;
+
+    @FindBy(id="userid")
+    WebElement userid;
+
+    @FindBy(id="password")
+    WebElement password;
+
     public HomePage(WebDriver driver) {
         super(driver);
         driver.get(path+"Controller?command=Login");
@@ -21,7 +30,15 @@ public class HomePage extends Page{
 
     public boolean isLoggedIn(){
         String tekst = paragraaf.getText();
-        return tekst.contains("welcome");
+        return tekst.contains("hallo");
+    }
+
+    public void logIn(String userId, String pass){
+        userid.clear();
+        userid.sendKeys(userId);
+        password.clear();
+        password.sendKeys(pass);
+        loginbutton.click();
     }
 
     public String getTekst(){
@@ -32,5 +49,17 @@ public class HomePage extends Page{
         button.click();
     }
 
+    public void refreshPage(){
+        driver.navigate().refresh();
+    }
+
+    public void goToOtherPage(){
+        driver.get(path+"Controller?command=Overview");
+    }
+
+    public void goBackToHome(){
+        driver.get(path+"Controller?command=Login");
+    }
 }
+
 
